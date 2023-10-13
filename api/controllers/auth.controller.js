60a7df2cabc1e159270ce50fff10fe7595f43561
sophoan <sophoan.meas@hotmail.comgit config --global user.email sophoan.meas@hotmail.comgit config --global user.email sophoan.meas@hotmail.com>
2023-10-13 01:53:16 -0400

    update

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -9,6 +9,7 @@ export const signup = async (req, res) => {
   try {
     await newUser.save();
     res.status(201).json('New user created successfully!');
+    
   } catch (error) {
     res.status(500).json(error.message);
   }
