45d07426e540e78ebab28d275992d362e7992ff0
sophoan <sophoan.meas@hotmail.comgit config --global user.email sophoan.meas@hotmail.comgit config --global user.email sophoan.meas@hotmail.com>
2023-10-15 20:00:37 -0400

    complete profile page ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 export default function Profile() {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
-    <div>
-      Profile
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <form className='flex flex-col'>
+        <img src={currentUser.avatar} alt='avatar'  className='rounded-full mt-2 h-24 w-24 object-cover cursor-pointer self-center'/>
+      <div className='mt-10 flex flex-col gap-3'>
+      <input type='text' placeholder='username' id='username' className='border p-2 rounded-lg'></input>
+      <input type='text' placeholder='email ' id='email' className='border p-2 rounded-lg'></input>
+      <input type='text' placeholder='password' id='password' className='border p-2 rounded-lg'></input>
+      <button className='bg-sky-700 text-white rounded-lg p-3 uppercase hover:opacity-80 disabled:opacity-80'>update</button>
+      </div>
+      </form>
+      <div className='flex justify-between mt-5'>
+        <span className='text-red-700 cursor-pointer'>Delete Account</span>
+        <span className='text-red-700 cursor-pointer'>Sing out</span>
+      </div>
     </div>
-  )
+  );
 }
