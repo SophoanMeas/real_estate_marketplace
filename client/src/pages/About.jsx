bd3ae152766ce51235efde99273ee9f5acca2085
sophoan <sophoan.meas@hotmail.com>
2023-10-29 17:50:29 -0400

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,37 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        About Sophoan Estate
+        </h1>
+        <p className='mb-4 text-slate-700'>
+        your premier destination for exquisite real estate offerings. 
+        With a legacy built on trust, excellence, and unmatched professionalism,
+        we are committed to bridging aspirational homeowners with their dream properties. 
+        Our team of seasoned real estate experts is dedicated to understanding your unique needs, 
+        curating tailored property options, and guiding you every step of the way, 
+        from initial browsing to final purchase. At Sophoan Estates, we believe in more than just transactions; 
+        we believe in crafting life-long memories in homes that resonate with your lifestyle and aspirations. 
+        Dive into our curated collection and let us help you find your next sanctuary.
+        </p>
+        <p className='mb-4 text-slate-700'>
+        For over two decades, Sophoan Estates has been at the forefront of the real estate industry, 
+        cementing our reputation as a beacon of reliability and excellence. Our unparalleled expertise 
+        is rooted in a deep understanding of the ever-evolving property landscape, coupled with our dedication 
+        to staying abreast of market trends and innovations. This ensures that we not only offer properties 
+        that stand the test of time but also investments that appreciate, reflecting our unwavering commitment 
+        to your long-term satisfaction and prosperity.
+        </p>
+        <p className='mb-4 text-slate-700'>
+        At the heart of Sophoan Estates is our belief in a personalized real estate experience. 
+        We recognize that every individual, family, or investor has a distinct vision of what &apos;home&apos; 
+        or &apos;investment&apos; means. By prioritizing a consultative approach, we immerse ourselves in your world, 
+        ensuring that every recommendation aligns seamlessly with your aspirations and financial goals. 
+        Our dedicated relationship managers, supported by a robust back-end team, are always on hand to provide insights, 
+        answer queries, and offer solutions, making your journey to property ownership as seamless and enjoyable as possible.
+        </p>
+
     </div>
   )
 }
